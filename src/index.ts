dd13ebf17249f08c7b96a98faba0c8cca990a893
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-29 12:15:57 +0100

    modfiying package json to look closer to the ngix reference file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+const result = sumScoutr(6);
+
 export default function sumScoutr(value: number): string {
   const summedValue = value + 100;
 
   return `Scoutr Version ${summedValue}`;
 }
 
-console.log(sumScoutr(2));
+console.log(result);
