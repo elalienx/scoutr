6a76d904e431c58645749163502303d6b48daf47
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-29 12:51:50 +0100

    make sure test works in this new setup

--- backend/src/index.test.ts
+++ backend/src/index.test.ts
@@ -15,3 +15,18 @@ test("To add 100 to the value passed", () => {
   // Assert
   expect(test).toBe(result);
 });
+
+test("Shows loser message if pass 0 or negative number", () => {
+  // Arrange
+  const result = "Such a looser!";
+  const valueA = 0;
+  const valueB = -5;
+
+  // Act
+  const testA = sumScoutr(valueA);
+  const testB = sumScoutr(valueB);
+
+  // Assert
+  expect(testA).toBe(result);
+  expect(testB).toBe(result);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
-const value: number = 8;
+const value: number = 5;
 
 export default function sumScoutr(value: number): string {
+  // safeguard
+  if (value <= 0) return "Such a looser!";
+
   const summedValue = value + 100;
 
   return `Scoutr Version ${summedValue}`;
