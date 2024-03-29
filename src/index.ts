64e423f84a2dac090fa924514ee3f8f4b49c99df
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-29 14:31:33 +0100

    installing node dependencies and setting up postgress in docker

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-const value: number = 3;
+// Properties
+const value: number = 4;
 
+// Methods
 export default function sumScoutr(value: number): string {
   // safeguard
   if (value <= 0) return "Such a looser!";
