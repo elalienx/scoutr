a59e73a009984c7b401550684166361d61f2a981
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-31 13:20:54 +0200

    removed previous space

--- src/transform/helpers/getText.ts
+++ src/transform/helpers/getText.ts
@@ -1,7 +1,6 @@
 // Node modules
 import { CheerioAPI } from "cheerio";
 
-
 export default function getText(document: CheerioAPI, selector: string): string {
   return document(selector).first().text().trim().substring(0, 50);
 }
