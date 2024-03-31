f81c37d46e16c0580c5bb10107372b6c6516a466
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-31 13:30:53 +0200

    adding test files

---
+++ src/transform/helpers/getText.test.ts
@@ -0,0 +1,15 @@
+// Node modules
+import { expect, test } from "vitest";
+
+// Project files
+import getText from "./getText";
+import 
+
+test("Returns a empty string in case the selector is not found, rather than crashing", () => {
+  // Arrange
+  const document = cheerio.load();
+  const result = "";
+
+  // Act
+  // Assert
+});
