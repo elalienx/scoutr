14f94dc2f48fe81cc141c7062ab8a8a9622a6f43
Eduardo <el_alienx@hotmail.com>
2024-03-31 21:25:58 +0200

    adding placeholders for the report

---
+++ src/report/reportEmptyFields.test.ts
@@ -0,0 +1,25 @@
+// Node modules
+import { expect, test } from "vitest";
+import reportEmptyFields from "./reportEmptyFields";
+import LinkedInProfile from "../types/LinkedInProfile";
+
+// Project files
+
+test("Create a full report mentioning every single field as missing", () => {
+  // Arrange
+  const profile: LinkedInProfile = {
+    candidate_name: "",
+    candidate_job_title: "",
+    candidate_image_url: "",
+    company_name: "",
+    company_duration_in_months: 0,
+    company_image_url: "",
+  };
+  const result = "";
+
+  // Act
+  const test = reportEmptyFields(profile);
+
+  // Assert
+  expect(test).toBe(result);
+});
---
+++ template.test.ts
@@ -0,0 +1,15 @@
+// Node modules
+import { expect, test } from "vitest";
+
+// Project files
+
+test("", () => {
+  // Arrange
+  const result = "";
+
+  // Act
+  const test = () => "";
+
+  // Assert
+  expect(test).toBe(result);
+});
