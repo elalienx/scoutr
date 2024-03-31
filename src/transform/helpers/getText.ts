7b13f5111bd98b68a559e3063dc4d90bb1708af7
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-31 13:20:27 +0200

    add space to test commit and sync

--- src/transform/helpers/getText.ts
+++ src/transform/helpers/getText.ts
@@ -1,6 +1,7 @@
 // Node modules
 import { CheerioAPI } from "cheerio";
 
+
 export default function getText(document: CheerioAPI, selector: string): string {
   return document(selector).first().text().trim().substring(0, 50);
 }
