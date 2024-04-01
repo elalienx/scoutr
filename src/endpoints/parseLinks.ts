b4586357a7a937485f8f1267a685417baceebaff
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-01 13:27:01 +0200

    preparing to create a new build

--- src/endpoints/parseLinks.ts
+++ src/endpoints/parseLinks.ts
@@ -6,7 +6,9 @@ import { Client } from "pg";
 import extractPage from "../extract/extractPage";
 import pageToProfile from "../transform/pageToProfile";
 import { insertCandidate } from "../sql-queries/insertCandidate";
+import { insertErrorLog } from "../sql-queries/insertErrorLog";
 import profileToCandidate from "../transform/profileToCandidate";
+import reportEmptyFields from "../reports/reportEmptyFields";
 
 export default async function parseLinkedInLinks(request: Request, response: Response, database: Client) {
   const { assignment_id } = request.params;
@@ -19,9 +21,15 @@ export default async function parseLinkedInLinks(request: Request, response: Res
     // Transform
     const profile = pageToProfile(page);
     const candidate = profileToCandidate(profile, { assignment_id, url });
+    const report = reportEmptyFields(url, profile);
+    const reportArray = Object.values(report);
 
     // Load
     const { rows } = await database.query(insertCandidate, candidate as unknown[]);
+    if (report.error_severity > 0) {
+      console.log(`Adding ${report.linked_in_url} to the ErrorLog table`);
+      await database.query(insertErrorLog, reportArray);
+    }
 
     return rows[0];
   }
---
+++ src/sql-queries/insertErrorLog.ts
@@ -0,0 +1,7 @@
+export const insertErrorLog: string = `INSERT INTO error_logs (
+    linked_in_url, 
+    error_severity, 
+    error_message,
+  ) 
+  VALUES ($1, $2, $3)
+`;
