21f14c6297ff10e1eed017f3f5e480695b64602a
Eduardo <el_alienx@hotmail.com>
2024-04-02 20:25:13 +0200

    finish adding resultsAPI to all endpoints

--- src/endpoints/postAssignment.ts
+++ src/endpoints/postAssignment.ts
@@ -4,17 +4,24 @@ import { Client } from "pg";
 
 // Project files
 import query from "../sql-queries/insertAssignment";
+import ResultsAPI from "../types/ResultsAPI";
 
-export default async function postAssignment(request: Request, response: Response, database: Client) {
+export default async function postAssignment(request: Request, response: Response, database: Client): Promise<void> {
   const { assignment_name, company_name, company_image_url } = request.body;
-  const values = [assignment_name, company_name, company_image_url];
-  const message = "Postgres added new assignment";
+  const messageGood = "Assignment saved with id ";
+  const messageBad = "Error: Cannot save data";
+  let result: ResultsAPI = { data: [], message: messageBad, status: 500 };
 
   try {
-    await database.query(query, values);
-    response.status(200).send({ message });
+    const { rows } = await database.query(query, [assignment_name, company_name, company_image_url]);
+    const assigment = rows[0];
+
+    result.data = assigment;
+    result.message = messageGood + assigment.id;
+    result.status = 200;
   } catch (error) {
     console.error(error);
-    response.sendStatus(500);
+  } finally {
+    response.status(result.status).send(result);
   }
 }
--- src/sql-queries/insertAssignment.ts
+++ src/sql-queries/insertAssignment.ts
@@ -1,6 +1,7 @@
 const insertAssignment = `
 INSERT INTO assignments (assignment_name, company_name, company_image_url) 
 VALUES ($1, $2, $3)
+RETURNING *
 `;
 
 export default insertAssignment;
