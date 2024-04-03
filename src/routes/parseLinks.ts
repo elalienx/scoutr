f52dc313bb78e14ee9159910f121b6bea1ac318d
Eduardo <el_alienx@hotmail.com>
2024-04-03 13:09:02 +0200

    align parse links to the way to return results using try/catch/finally like other routes

--- src/routes/parseLinks.ts
+++ src/routes/parseLinks.ts
@@ -5,20 +5,23 @@ import { Client } from "pg";
 // Project files
 import etlProcess from "../extract-transform/etlProcess";
 import packageResults from "../extract-transform/package-results/packageResults";
+import ResultsAPI from "../types/ResultsAPI";
 
 export default async function parseLinks(request: Request, response: Response, database: Client) {
   const assignment_id = Number(request.params.assignment_id);
   const links: string[] = request.body.links;
+  const messageBad = "Error: Cannot parse links";
+  let result: ResultsAPI = { data: [], message: messageBad, status: 500 };
 
   try {
     const etl = await Promise.all(links.map((link) => etlProcess(link, assignment_id, database)));
     const candidates = etl.map((item) => item.candidate);
     const reports = etl.map((item) => item.report);
-    const results = packageResults(candidates, reports);
 
-    response.status(200).send(results);
+    result = packageResults(candidates, reports);
   } catch (error) {
     console.error(error);
-    response.sendStatus(500);
+  } finally {
+    response.status(result.status).send(result);
   }
 }
