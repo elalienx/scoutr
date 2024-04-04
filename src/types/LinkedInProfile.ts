613d6546a6b7853d9ebf90253d13339972f9b825
Eduardo <el_alienx@hotmail.com>
2024-04-04 21:37:53 +0200

    removed usused type and documentation not longer needed on LinkedInProfile

--- src/types/LinkedInProfile.ts
+++ src/types/LinkedInProfile.ts
@@ -1,10 +1,3 @@
-/**
- * About:
- * This interface outlines the data extracted from a LinkedIn user.
- * But for a complete candidate profile, more information is required.
- * So we need an array interface named Candidate to add the missing
- * information and ensure compatibility with PostgreSQL insert queries.
- */
 export default interface LinkedInProfile {
   candidate_name: string;
   candidate_job_title: string;
