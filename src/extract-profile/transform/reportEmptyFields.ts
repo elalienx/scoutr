771ee37978c870d2e9c2c1ea8d15b64629404a63
Eduardo <el_alienx@hotmail.com>
2024-04-06 22:28:14 +0200

    small refactor in report Empty fields

--- src/extract-profile/transform/reportEmptyFields.ts
+++ src/extract-profile/transform/reportEmptyFields.ts
@@ -5,26 +5,26 @@ import LinkedInProfile from "../../types/LinkedInProfile";
 export default function reportEmptyFields(linked_in_url: string, profile: LinkedInProfile): ErrorReport {
   const fields = Object.entries(profile);
   const missingFields: string[] = [];
-  let error_severity = 0;
-  let error_message = "No problems found";
+  let severity = 0;
+  let message = "No problems found";
 
   for (let [key, value] of fields) {
     if (!value) missingFields.push(" " + key);
   }
 
   if (missingFields.length > 0) {
-    error_message = "Missing" + missingFields;
-    error_severity = 1;
+    message = "Missing" + missingFields;
+    severity = 1;
   }
 
   if (missingFields.length == fields.length) {
-    error_message = "Missing all fields";
-    error_severity = 2;
+    message = "Missing all fields";
+    severity = 2;
   }
 
   return {
     linked_in_url,
-    severity: error_severity,
-    message: error_message,
+    severity,
+    message,
   };
 }
