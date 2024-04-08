de72cf331264260f5924228aa76d7d3b3f8e6395
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-08 15:11:59 +0200

    remove empty line

--- src/extract-profile/load/packageResults.ts
+++ src/extract-profile/load/packageResults.ts
@@ -5,7 +5,6 @@ import ResultsAPI from "../../types/ResultsAPI";
 export default function packageResults(candidates: object[], reports: ErrorReport[]): ResultsAPI {
   const severeReports = reports.filter((item) => item.severity > 0);
   const hasCandidates = candidates.length > 0;
-
   let status = 500;
   let data = [];
   let message: string | unknown[] = "Error parsing links";
