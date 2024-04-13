bdda33625882ba0477c7248ee1019cc8f8068e10
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-13 16:06:43 +0200

    Extracted CSS classes on pageToProfile

--- src/extract-profile/transform/pageToProfile.ts
+++ src/extract-profile/transform/pageToProfile.ts
@@ -10,16 +10,20 @@ import getText from "./helpers/getText";
 
 export default function pageToProfile(page: string): LinkedInProfile {
   const document: CheerioAPI = CheerioLoad(page);
-  const company: string = ".top-card-layout__entity-info--right-column";
-  const jobTitle: string = getText(document, "h2.top-card-layout__headline");
-  const duration: string = getText(document, "ul.experience__list li .date-range span");
+  const css_candidate_name = "h1.top-card-layout__title";
+  const css_job_title = "h2.top-card-layout__headline";
+  const css_candidate_image = ".top-card__profile-image";
+  const css_company = ".top-card-layout__entity-info--right-column";
+  const css_duration = "ul.experience__list li .date-range span";
+  const jobTitle: string = getText(document, css_job_title);
+  const duration: string = getText(document, css_duration);
 
   return {
-    candidate_name: getText(document, "h1.top-card-layout__title"),
+    candidate_name: getText(document, css_candidate_name),
     candidate_job_title: getPrimaryJobTitle(jobTitle),
-    candidate_image_url: getImage(document, ".top-card__profile-image"),
-    company_name: getText(document, `${company} span`),
+    candidate_image_url: getImage(document, css_candidate_image),
+    company_name: getText(document, `${css_company} span`),
     company_duration_in_months: getDurationInMonths(duration),
-    company_image_url: getImage(document, `${company} img`),
+    company_image_url: getImage(document, `${css_company} img`),
   };
 }
