296736e8bad2d9c29420850e8b870259a2cace50
Eduardo <el_alienx@hotmail.com>
2024-04-25 17:37:04 +0200

    added fe files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+// Node modules
+import { useState } from "react";
+
+// Project files
+import "./App.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  
+
+  return <div>
+
+  </div>;
+}
