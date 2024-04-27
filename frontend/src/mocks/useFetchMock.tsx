45f49bbbb36e36c8d857ba020f5cd3e3852c9f4c
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-27 13:24:50 +0200

    adding first tests

--- frontend/src/mocks/useFetchMock.tsx
+++ frontend/src/mocks/useFetchMock.tsx
@@ -8,6 +8,41 @@ export default function useFetchMock(url: string) {
       return { data: [], status: "empty", message: "" };
     case "www.content.com":
       return { data: [1, 2, 3], status: "content", message: "" };
+    case "www.content.com/assignments":
+      return {
+        data: [
+          {
+            id: 1,
+            date_created: "2024-01-31 21:00:30.610279",
+            assignment_name: "Data Engineer",
+            company_name: "Folksam",
+            company_image_url: "",
+          },
+          {
+            id: 2,
+            date_created: "2024-02-01 21:00:30.610279",
+            assignment_name: "Developer Architect",
+            company_name: "Folksam",
+            company_image_url: "",
+          },
+          {
+            id: 3,
+            date_created: "2024-02-02 21:00:30.610279",
+            assignment_name: "Master Data Specialist",
+            company_name: "McDonalds",
+            company_image_url: "",
+          },
+          {
+            id: 4,
+            date_created: "2024-02-02 21:00:30.610279",
+            assignment_name: "Pega Lead System Architech",
+            company_name: "Nordea Bank",
+            company_image_url: "",
+          },
+        ],
+        status: "content",
+        message: "",
+      };
     default:
       throw new Error(`Case url: ${url} not found`);
   }
--- frontend/src/pages/assignments/Assignments.test.tsx
+++ frontend/src/pages/assignments/Assignments.test.tsx
@@ -6,7 +6,7 @@ import { render, screen } from "@testing-library/react";
 import Assignments from "./Assignments";
 import useFetchMock from "mocks/useFetchMock";
 
-test("Expect loading to be in document", () => {
+test("Expect loading state", () => {
   // Arrange
   const customHook = useFetchMock("www.loading.com");
   render(<Assignments customHook={customHook} />);
@@ -17,4 +17,54 @@ test("Expect loading to be in document", () => {
   // Assert
   // @ts-ignore
   expect(test).toBeInTheDocument();
+});
+
+test("Expect error state", () => {
+  // Arrange
+  const customHook = useFetchMock("www.error.com");
+  render(<Assignments customHook={customHook} />);
+
+  // Act
+  const test = screen.queryByText("Oh no! We could not load any assigment.");
+
+  // Assert
+  // @ts-ignore
+  expect(test).toBeInTheDocument();
+});
+
+test("Expect emtpy state", () => {
+  // Arrange
+  const customHook = useFetchMock("www.empty.com");
+  render(<Assignments customHook={customHook} />);
+
+  // Act
+  const test = screen.queryByText(
+    "Seems like you haven’t created any assigments yet."
+  );
+
+  // Assert
+  // @ts-ignore
+  expect(test).toBeInTheDocument();
+});
+
+test("Expect content state", () => {
+  // Arrange
+  const customHook = useFetchMock("www.content.com/assignments");
+  render(<Assignments customHook={customHook} />);
+
+  // Act
+  const test1 = screen.queryByText("Data Engineer");
+  const test2 = screen.queryByText("Developer Architect");
+  const test3 = screen.queryByText("Master Data Specialist");
+  const test4 = screen.queryByText("New assignment");
+
+  // Assert
+  // @ts-ignore
+  expect(test1).toBeInTheDocument();
+  // @ts-ignore
+  expect(test2).toBeInTheDocument();
+  // @ts-ignore
+  expect(test3).toBeInTheDocument();
+  // @ts-ignore
+  expect(test4).toBeInTheDocument();
 });
