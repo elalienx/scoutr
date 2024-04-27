ee027e2ce922937eb2917d46bdba8efd0a40e3df
Eduardo Alvarez <el_alienx@hotmail.com>
2024-04-27 15:55:23 +0200

    removed comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 // Project files
 import Assignments from "pages/assignments/Assignments";
 import useFetch from "hooks/useFetch";
-import useFetchMock from "mocks/useFetchMock";
 
 export default function App() {
-  // useFetchMock(www.error.com);
-  // useFetch("/api/assignments", [])
-
   return (
     <div>
       <Assignments customHook={() => useFetch("/api/assignments")} />
