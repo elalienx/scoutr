935b27b6a920df89ff2afc920ce01174bc4a9a6c
Eduardo <el_alienx@hotmail.com>
2024-05-05 22:00:36 +0200

    adding next tests to add

--- frontend/src/components/input-fields/InputFields.test.tsx
+++ frontend/src/components/input-fields/InputFields.test.tsx
@@ -3,3 +3,21 @@ import { expect, test } from "vitest";
 
 // Project files
 import InputFields from "./InputFields";
+import { render, screen } from "scripts/testing-library-globals";
+
+test("Returns warning message if nothing is passed", () => {
+  // Arrange
+  const fields = [];
+  render(<InputFields fields={fields} />);
+
+  // Act
+  const test = screen.getByText("No fields passed");
+
+  // Assert
+  // @ts-ignore
+  expect(test).toBeInTheDocument();
+});
+
+test.todo("Return 1 input field when passed data");
+test.todo("Return 2 input fields when passed data");
+test.todo("Return 1 input field and 1 textare when passed data");
--- frontend/src/components/input-fields/InputFields.tsx
+++ frontend/src/components/input-fields/InputFields.tsx
@@ -1,1 +1,9 @@
-export default function InputFields() {}
+interface Props {
+  fields: any[];
+}
+
+export default function InputFields({ fields }: Props) {
+  if (fields.length === 0) return <small>No fields passed</small>;
+
+  return <></>;
+}
