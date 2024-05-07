d1b16f2d6c76519000787c7b72a94a68c30b8f56
Eduardo <el_alienx@hotmail.com>
2024-05-07 23:08:19 +0200

    fix name issue in form candidates

--- frontend/src/pages/candidates/form-candidates/FormCandidates.tsx
+++ frontend/src/pages/candidates/form-candidates/FormCandidates.tsx
@@ -37,11 +37,12 @@ export default function FormCandidates({ id, state }: Props) {
   // Methods
   async function onSubmit(event: FormEvent<HTMLFormElement>) {
     const formData = gatherFormData(event.currentTarget);
-    const links = textAreaToArray(formData.links);
+    const links = textAreaToArray(formData.unparsed_links);
+    const body = { links };
     const options = {
       headers: { "Content-Type": "application/json" },
       method: "POST",
-      body: JSON.stringify(links),
+      body: JSON.stringify(body),
     };
 
     event.preventDefault();
