c1cf241188cd82db152efb964ef508447a469cce
Eduardo <el_alienx@hotmail.com>
2024-05-09 22:41:28 +0200

    fix tests

--- frontend/src/mocks/fetchAssignment.test.ts
+++ frontend/src/mocks/fetchAssignment.test.ts
@@ -6,14 +6,14 @@ import fetchAssignment from "./fetchAssignment";
 import Assignment from "types/Assignment";
 import packageData from "scripts/packageData";
 
-test("Show error is API is not correct", async () => {
+test("Show error if URI is not correct", async () => {
   // Arrange
   const uri = "api/assigments/"; // missing the "n" between "g" and "m" on purpose
-  const data = {
+  const formData = {
     assignment_name: "Graphic Designer",
     company_name: "Spotify",
   };
-  const options = packageData("POST", data);
+  const options = packageData("POST", formData);
   // prettier-ignore
   const result = "The URI appears invalid. Check for typos or update the test for any endpoint changes."
 
@@ -24,14 +24,14 @@ test("Show error is API is not correct", async () => {
   expect(test).rejects.toThrowError(result);
 });
 
-test.todo("Show error if options are not valid", () => {
+test("Show error if form data is not valid", () => {
   // Arrange
   const uri = "/api/assignments";
-  const data = {
+  const formData = {
     assignment_name: "Graphic Designer",
     company: "Spotify", // company should be company_name
   };
-  const options = packageData("POST", data);
+  const options = packageData("POST", formData);
   // prettier-ignore
   const result = "The data send to the server appears invalid. Check for typos or update the test for any endpoint changes."
 
@@ -42,16 +42,16 @@ test.todo("Show error if options are not valid", () => {
   expect(test).rejects.toThrowError(result);
 });
 
-test("Verifies that recives the correct URI from the form", () => {
+test("Returns a valid assignment with ID if everything is correct", async () => {
   // Arrange
-  const uri = "api/assignments/";
+  const uri = "/api/assignments";
   const data = {
     assignment_name: "Graphic Designer",
     company_name: "Spotify",
   };
   const options = packageData("POST", data);
   const result: Assignment = {
-    id: 1,
+    id: 9999,
     date_created: "2024-12-31",
     assignment_name: "Graphic Designer",
     company_name: "Spotify",
@@ -59,10 +59,8 @@ test("Verifies that recives the correct URI from the form", () => {
   };
 
   // Act
-  const test = Promise.resolve(fetchAssignment(uri, options));
+  const test = await fetchAssignment(uri, options);
 
   // Assert
   expect(test.data).toEqual(result);
 });
-
-test.todo("Verifies that receives the correct data from the form", () => {});
--- frontend/src/mocks/fetchAssignment.ts
+++ frontend/src/mocks/fetchAssignment.ts
@@ -3,8 +3,8 @@ import Assignment from "types/Assignment";
 import ResultsAPI from "types/ResultsAPI";
 
 async function fetchAssignment(uri: string, init: object): Promise<ResultsAPI> {
-  // console.log(init);
   const { body } = init;
+  const data = JSON.parse(body);
 
   // Safeguards
   // prettier-ignore
@@ -13,16 +13,16 @@ async function fetchAssignment(uri: string, init: object): Promise<ResultsAPI> {
   const errorURI = "The URI appears invalid. Check for typos or update the test for any endpoint changes.";
 
   if (uri !== "/api/assignments") throw new Error(errorURI);
-  if (body.hasOwnProperty("assignment_name")) throw new Error(errorInit);
-  if (body.hasOwnProperty("company_name")) throw new Error(errorInit);
+  if (!data.hasOwnProperty("assignment_name")) throw new Error(errorInit);
+  if (!data.hasOwnProperty("company_name")) throw new Error(errorInit);
 
   // Properties
   const assignment: Assignment = {
     id: 9999,
-    date_created: "2024-01-31",
-    assignment_name: body.assignment_name,
-    company_name: body.company_name,
-    company_image_url: "some-image.png",
+    date_created: "2024-12-31",
+    assignment_name: data.assignment_name,
+    company_name: data.company_name,
+    company_image_url: "",
   };
   const result: ResultsAPI = {
     data: assignment,
