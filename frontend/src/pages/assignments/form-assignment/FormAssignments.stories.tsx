f35af5d69052e0a5e77845d1fe5dd972ca64b033
Eduardo <el_alienx@hotmail.com>
2024-05-09 15:44:33 +0200

    adding story

---
+++ frontend/src/pages/assignments/form-assignment/FormAssignments.stories.tsx
@@ -0,0 +1,32 @@
+// Node modules
+import type { Meta, StoryObj } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
+
+// Project files
+import FormAssignment from "./FormAssignment";
+
+const meta = {
+  title: "Formularies/New Assignment",
+  component: FormAssignment,
+  parameters: { layout: "centered" },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
+  tags: ["autodocs"],
+} satisfies Meta<typeof FormAssignment>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// Stories
+export const Default: Story = {
+  args: {},
+};
+
+export const Loading: Story = {
+  args: {},
+};
