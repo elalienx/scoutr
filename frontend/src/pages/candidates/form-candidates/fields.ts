7802b248c9a5ac90d4b4bbbbd1d0c3efd9f4560c
Eduardo <el_alienx@hotmail.com>
2024-05-11 11:50:58 +0200

    improve micro copy

--- frontend/src/pages/candidates/form-candidates/fields.ts
+++ frontend/src/pages/candidates/form-candidates/fields.ts
@@ -5,9 +5,8 @@ const fields: InputField[] = [
   {
     id: "unparsed_links",
     type: "text-area",
-    label: "Paste the LinkedIn links here",
-    placeholder:
-      "https://www.linkedin.com/in/eduardo-alvarez-nowak/\nhttps://www.linkedin.com/in/lanahaddad87/\nhttps://www.linkedin.com/in/susanna-vaara-0b33b03a/",
+    label: "Paste one or more LinkedIn links here",
+    placeholder: "https://www.linkedin.com/in/eduardo-alvarez-nowak/\nhttps://www.linkedin.com/in/lanahaddad87/",
     defaultValue: "",
     required: true,
     description: "Only paste links and delete any other type of text.",
