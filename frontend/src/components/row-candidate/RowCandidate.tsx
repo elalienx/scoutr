c5aa86496be4b8c9e7170c01c3cfc75633197582
Eduardo <el_alienx@hotmail.com>
2024-05-18 23:30:06 +0200

    removed incomplete feature being developer in other branch

--- frontend/src/components/row-candidate/RowCandidate.tsx
+++ frontend/src/components/row-candidate/RowCandidate.tsx
@@ -6,7 +6,6 @@ import ItemCompany from "components/item-company/ItemCompany";
 import Candidate from "types/Candidate";
 import parseData from "./helpers/parseData";
 import "./row-candidate.css";
-import useDialog from "state/DialogContextAPI";
 
 interface Props {
   /** The candidate to present */
@@ -20,16 +19,8 @@ interface Props {
 export default function RowCandidate({ candidate, index }: Props) {
   const { notes, relevance, contact_status } = candidate;
 
-  // Global state
-  const { showDialog } = useDialog();
-
   // Properties
   const parsedData = parseData(candidate, index);
-
-  // Methods
-  function onClick(key: string) {
-    showDialog(<p className="form">You clicked on {key}</p>);
-  }
 
   return (
     <tr className="row-candidate">
@@ -45,7 +36,7 @@ export default function RowCandidate({ candidate, index }: Props) {
       <td className="company column-big" data-label="Company">
         <ItemCompany {...candidate} />
       </td>
-      <td onClick={() => onClick("notes")} className="notes column-big" data-label="Notes">
+      <td className="notes column-big" data-label="Notes">
         <small className="trim-text">{notes}</small>
       </td>
       <td className="relevance column-medium" data-label="Relevance">
