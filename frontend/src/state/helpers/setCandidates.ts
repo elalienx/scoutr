3bded078606fadf622a706e587428b72d0f8e753
Eduardo <el_alienx@hotmail.com>
2024-05-21 16:08:30 +0200

    removed broken code

--- frontend/src/state/helpers/setCandidates.ts
+++ frontend/src/state/helpers/setCandidates.ts
@@ -2,11 +2,5 @@
 import Candidate from "types/Candidate";
 
 export default function set(payload: Candidate[]) {
-  // Properties
-  const error = "You cannot clear the candidate state with this command.";
-
-  // safeguard
-  if (payload.length === 0) throw new Error(error);
-
   return payload;
 }
