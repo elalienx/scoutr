ea3c88876e7d9be4f216bfa8be39bc17f846e7c7
Eduardo <el_alienx@hotmail.com>
2024-05-24 15:08:46 +0200

    added vertical pipe as a cut character in job title

--- backend/src/extract-profile/transform/helpers/getPrimaryJob.ts
+++ backend/src/extract-profile/transform/helpers/getPrimaryJob.ts
@@ -1,5 +1,5 @@
 export default function getPrimaryJobTitle(text: string): string {
-  const cutWords: RegExp = /,| at | på |@/i;
+  const cutWords: RegExp = /,| at | på |\||@/i;
   const phrase = text.split(cutWords);
   const result = phrase[0].trim();
 
