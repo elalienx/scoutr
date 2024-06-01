ab01dcae3d19b5ac4dac8b68f1e00425c43bf413
Eduardo <el_alienx@hotmail.com>
2024-06-01 10:53:57 +0200

    adding back headers

--- backend/src/routes/parseLinks.ts
+++ backend/src/routes/parseLinks.ts
@@ -9,10 +9,14 @@ import decodeLinks from "../scripts/decodeLinks";
 export default async function parseLinks(request: Request, response: Response, database: Client) {
   // Headers
   response.setHeader("Content-Type", "text/event-stream");
+  response.setHeader("Cache-Control", "no-cache");
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Connection", "keep-alive");
 
   // Properties
   const assignment_id = Number(request.params.assignment_id);
   const links = decodeLinks(request.query.links as string);
+  console.log("backend links:", links);
 
   try {
     for (const link of links) {
