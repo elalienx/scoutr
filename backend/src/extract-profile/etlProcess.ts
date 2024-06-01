273c529d2e7d9895093f6ae17f865e6d71a25ad8
Eduardo <el_alienx@hotmail.com>
2024-06-01 14:47:05 +0200

    22

--- backend/src/extract-profile/etlProcess.ts
+++ backend/src/extract-profile/etlProcess.ts
@@ -4,9 +4,11 @@ import type { Client } from "pg";
 // Project files
 import candidateQuery from "../queries/insertCandidate";
 import reportQuery from "../queries/insertErrorLog";
+import type ErrorReport from "../types/ErrorReport";
 import extractPage from "./extract/extractPage";
 import pageToProfile from "./transform/pageToProfile";
 import reportEmptyFields from "./transform/reportEmptyFields";
+import reportPrivateProfile from "./transform/reportPrivateProfile";
 
 export default async function etlProcess(url: string, assignment_id: number, database: Client) {
   // Extract
@@ -14,12 +16,16 @@ export default async function etlProcess(url: string, assignment_id: number, dat
 
   // Transform
   const profile = pageToProfile(page);
-  const report = reportEmptyFields(url, profile);
-  const profileAsArray = [assignment_id, url, ...Object.values(profile)];
-  const reportAsArray = Object.values(report);
+  let report = reportEmptyFields(url, profile);
 
+  if (report.severity === 2) report = reportPrivateProfile(url, page);
+
+
+  
   // Load
   let candidate = {};
+  const profileAsArray = [assignment_id, url, ...Object.values(profile)];
+  const reportAsArray = Object.values(report);
 
   if (report.severity < 2) candidate = (await database.query(candidateQuery, profileAsArray)).rows[0];
   if (report.severity) await database.query(reportQuery, reportAsArray);
