2c89c272ecd02378bda34e3f1480e7f7bfea8c1b
Eduardo <el_alienx@hotmail.com>
2024-06-12 15:39:26 +0200

    adding stealth features

--- backend/src/extract-profile/extract/extractPage.ts
+++ backend/src/extract-profile/extract/extractPage.ts
@@ -1,14 +1,18 @@
 // Node modules
-import { webkit as navigator } from "playwright";
+import { webkit as navigator } from "playwright-extra";
+import stealth from "puppeteer-extra-plugin-stealth";
 
 export default async function extractPage(url: string): Promise<string> {
+  // activate stealth
+  navigator.use(stealth());
+
   const browser = await navigator.launch();
   const page = await browser.newPage();
   let result = "";
 
   try {
-    await page.goto(url);
-    await page.waitForSelector("body");
+    await page.goto(url, { waitUntil: "networkidle" });
+    await page.screenshot({ path: "stealth.png", fullPage: true });
 
     result = await page.content();
   } catch (error) {
