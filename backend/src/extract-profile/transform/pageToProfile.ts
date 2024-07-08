78ff57f8d0bce47ac386ced2817ec806a624a05c
Eduardo <el_alienx@hotmail.com>
2024-07-08 17:15:21 +0200

    fix the order of the import

--- backend/src/extract-profile/transform/pageToProfile.ts
+++ backend/src/extract-profile/transform/pageToProfile.ts
@@ -19,9 +19,9 @@ export default function pageToProfile(page: string): LinkedInProfile {
 
   return {
     candidate_name: document("h1").text(),
+    candidate_job_title: purpleDocument('span.t-14.t-normal:first > span[aria-hidden="true"]').text(),
     candidate_image_url: document(".EntityPhoto-circle-9 > img").attr("src"),
     company_name: purpleDocument('div.display-flex.align-items-center.mr1.t-bold > span[aria-hidden="true"]').text(),
-    candidate_job_title: purpleDocument('span.t-14.t-normal:first > span[aria-hidden="true"]').text(),
     company_duration_in_months: 0,
     company_image_url: orangeDocument("img").attr("src"),
   };
