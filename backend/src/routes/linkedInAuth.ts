5d10cd9e7b453e6ff247bc3e88abe7595c8eb70a
Eduardo <el_alienx@hotmail.com>
2024-07-12 16:43:59 +0200

    fix the endpint

--- backend/src/routes/linkedInAuth.ts
+++ backend/src/routes/linkedInAuth.ts
@@ -3,7 +3,7 @@ import type { Request, Response } from "express";
 import getAuth from "../auth/getAuth";
 
 export default async function linkedInAuth(request: Request, response: Response): Promise<void> {
-  const { email, password } = request.params;
+  const { email, password } = request.body;
   const messageBad = "Error: Can't generate auth";
   const statusGood = 200;
   console.log("getLinkedInAuth email and password", email, password);
