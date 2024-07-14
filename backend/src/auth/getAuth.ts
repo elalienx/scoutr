05118c6b6657d0dd57a6cb8d6fe1df5df04b0aa5
Eduardo <el_alienx@hotmail.com>
2024-07-14 23:31:22 +0200

    removing unused code and adding notes for next sprint

--- backend/src/auth/getAuth.ts
+++ backend/src/auth/getAuth.ts
@@ -3,6 +3,10 @@ import { stdin as input, stdout as output } from "node:process";
 import readline from "readline/promises";
 import { firefox as navigator } from "playwright";
 
+/**
+ * Refactor, each comment title is a potential new method.
+ * Including prompt, should be a separate file
+ */
 async function authAdvanced(): Promise<void> {
   // Launch browser
   const browser = await navigator.launch();
@@ -15,29 +19,27 @@ async function authAdvanced(): Promise<void> {
 
   // Navigate to the login page
   await page.goto("https://www.linkedin.com/login");
-  await page.screenshot({ path: "screenshots/login-advanced-1A-before.png" });
 
   // Fill in login details and submit
   await page.fill("#username", email);
   await page.fill("#password", password);
-  await page.screenshot({ path: "screenshots/login-advanced-1B-after.png" });
+  await page.screenshot({ path: "screenshots/login-advanced-1.png" });
   await page.click("button[aria-label='Sign in']");
 
   // Wait for the security check prompt (this selector should match the element that asks for the code)
   await page.waitForSelector("#input__email_verification_pin");
-  await page.screenshot({ path: "screenshots/login-advanced-2A-before.png" });
 
   // Prompt user to enter the code
   const code = await prompt("Enter the security code: ");
 
   // Fill in the security code
   await page.fill("#input__email_verification_pin", code);
-  await page.screenshot({ path: "screenshots/login-advanced-2B-after.png" });
+  await page.screenshot({ path: "screenshots/login-advanced-2.png" });
   await page.click("#email-pin-submit-button");
 
   // Save auth
-  await page.screenshot({ path: "screenshots/login-advanced-3-complete.png" });
   await page.context().storageState({ path: "src/auth/loginAuth.json" });
+  await page.screenshot({ path: "screenshots/login-advanced-3.png" });
 
   // Close the browser
   await browser.close();
