8ddd3096c9fbfe86742c2161a8bb0971d49a767f
Eduardo <el_alienx@hotmail.com>
2024-07-18 11:12:38 +0200

    removed console logs

--- backend/src/extract-profile/extract/extractPage.ts
+++ backend/src/extract-profile/extract/extractPage.ts
@@ -2,30 +2,22 @@
 import { firefox as navigator } from "playwright";
 
 export default async function extractPage(url: string): Promise<string> {
-  console.time("Open browser");
   const browser = await navigator.launch();
   const context = await browser.newContext({ storageState: "src/auth/loginAuth.json" });
   const page = await context.newPage();
   let result = "";
-  console.timeLog("Open browser");
 
   try {
-    console.time("Go to URL");
     await page.goto(url);
     await page.waitForSelector(".pvs-header__title");
-    console.timeLog("Go to URL");
 
-    console.time("Return page");
     result = await page.content();
-    console.timeLog("Return page");
   } catch (error) {
     await page.screenshot({ path: "screenshots/error.png", fullPage: true });
     console.error(`Playwright: Cant' navigate to URL "${url}"`);
   } finally {
-    console.time("Close browser");
     await context.close();
     await browser.close();
-    console.timeLog("Close browser");
   }
 
   return result;
