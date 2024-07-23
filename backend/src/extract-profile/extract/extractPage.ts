c72d54890070f8c2eb7d76347479db0dd354a5a7
Eduardo <el_alienx@hotmail.com>
2024-07-23 19:47:23 +0200

    added code to trigger errrors

--- backend/src/extract-profile/extract/extractPage.ts
+++ backend/src/extract-profile/extract/extractPage.ts
@@ -3,11 +3,12 @@ import { Page } from "playwright";
 
 export default async function extractPage(page: Page, url: string): Promise<string> {
   // Properties
+  const timeout10Seconds = 10000;
   let result = "";
 
   try {
     await page.goto(url, { waitUntil: "domcontentloaded" });
-    await page.waitForSelector(".pvs-header__title");
+    await page.waitForSelector(".pvs-header__title", { timeout: timeout10Seconds });
 
     await page.screenshot({ path: "screenshots/debug.png", fullPage: true });
     result = await page.content();
