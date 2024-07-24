80b171a4583ac01a5e8ee71b9d7bbe96f274fd17
Eduardo <el_alienx@hotmail.com>
2024-07-24 14:58:28 +0200

    removed unused method

--- backend/src/extract-profile/transform/helpers/getText.ts
+++
@@ -1,6 +0,0 @@
-// Node modules
-import type { CheerioAPI } from "cheerio";
-
-export default function getText(document: CheerioAPI, selector: string): string {
-  return document(selector).first().text().trim();
-}
