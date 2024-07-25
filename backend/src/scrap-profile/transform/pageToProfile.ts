e9a86133dd7210ebf63b10a497a08aec8a570194
Eduardo <el_alienx@hotmail.com>
2024-07-25 15:19:44 +0200

    preparing for unit test

--- backend/src/scrap-profile/transform/pageToProfile.ts
+++ backend/src/scrap-profile/transform/pageToProfile.ts
@@ -23,7 +23,7 @@ export default function pageToProfile(page: string): LinkedInProfile {
 
   // Helpers
   const removeTypeOfJob: RegExp = / · .*/; // Novare · Full time = Novare. ACdelco · Contract = ACdelco, etc.
-  const maximumDatabaseSize = 999999;
+  const maximumDatabaseSize = 50;
 
   // Extra transformations
   // -- 1. Candidate name
