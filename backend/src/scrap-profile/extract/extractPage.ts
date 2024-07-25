5a999b45da6f7b1895d4e4f517ba0c6aa97e4e0d
Eduardo <el_alienx@hotmail.com>
2024-07-25 16:12:59 +0200

    adding debug code

--- backend/src/scrap-profile/extract/extractPage.ts
+++ backend/src/scrap-profile/extract/extractPage.ts
@@ -2,6 +2,7 @@
 import { Page } from "playwright";
 
 export default async function extractPage(page: Page, url: string): Promise<string> {
+  console.time("extractPage");
   // Properties
   const timoutInMilliseconds = 10_000;
   let result = "";
@@ -14,7 +15,12 @@ export default async function extractPage(page: Page, url: string): Promise<stri
   } catch (error) {
     await page.screenshot({ path: "screenshots/error.png", fullPage: true });
     throw new Error(`Playwright: Cant' navigate to URL "${url}". The auth is probably expired.`);
+  } finally {
+    console.log("'\x1b[32m%s\x1b[0m'", "=== FINISHED SCRAPING PAGE ===");
+    page.goto("about:blank"); // to stop linkedin ad scripts eating resources
   }
 
+  console.timeLog("extractPage");
+  console.timeEnd("extractPage");
   return result;
 }
