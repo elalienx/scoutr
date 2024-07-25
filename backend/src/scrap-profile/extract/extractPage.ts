b35091b1b182fd223f5696677ec40344aa061aec
Eduardo <el_alienx@hotmail.com>
2024-07-25 16:24:24 +0200

    first implementation of defenses against LinkedIn adware

--- backend/src/scrap-profile/extract/extractPage.ts
+++ backend/src/scrap-profile/extract/extractPage.ts
@@ -2,7 +2,6 @@
 import { Page } from "playwright";
 
 export default async function extractPage(page: Page, url: string): Promise<string> {
-  console.time("extractPage");
   // Properties
   const timoutInMilliseconds = 10_000;
   let result = "";
@@ -16,11 +15,8 @@ export default async function extractPage(page: Page, url: string): Promise<stri
     await page.screenshot({ path: "screenshots/error.png", fullPage: true });
     throw new Error(`Playwright: Cant' navigate to URL "${url}". The auth is probably expired.`);
   } finally {
-    console.log("'\x1b[32m%s\x1b[0m'", "=== FINISHED SCRAPING PAGE ===");
-    page.goto("about:blank"); // to stop linkedin ad scripts eating resources
+    page.goto("about:blank"); // Important: To stop linkedin ad scripts eating resources
   }
 
-  console.timeLog("extractPage");
-  console.timeEnd("extractPage");
   return result;
 }
