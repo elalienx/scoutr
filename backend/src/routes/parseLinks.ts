10c0385329b93aef2099e4274dd647433627d949
Eduardo <el_alienx@hotmail.com>
2024-08-05 12:07:27 +0200

    remove timer code

--- backend/src/routes/parseLinks.ts
+++ backend/src/routes/parseLinks.ts
@@ -8,7 +8,6 @@ import etlProcess from "../scan-profile/etlProcess";
 import unZipLinks from "../scripts/unZipLinks";
 
 export default async function parseLinks(request: Request, response: Response, database: Client, browserPage: Page) {
-  console.time("parse-links");
   // Headers
   response.setHeader("Content-Type", "text/event-stream");
 
@@ -32,5 +31,4 @@ export default async function parseLinks(request: Request, response: Response, d
     response.end();
     browserPage.goto("about:blank"); // Important: To stop linkedin ad scripts eating resources
   }
-  console.timeEnd("parse-links");
 }
