fcce9d3c5f63c1d659a6ba4d8eb46a65ebee79b4
Eduardo <el_alienx@hotmail.com>
2024-08-07 19:27:56 +0200

    added 1 test in DB

--- backend/src/database/postgresClient.ts
+++ backend/src/database/postgresClient.ts
@@ -6,21 +6,22 @@ import type Credentials from "../types/DatabaseCredentials";
 
 export default async function postgresClient(credentials: Credentials): Promise<pg.Client> {
   const client: pg.Client = new pg.Client(credentials);
+  console.log("creating DB class does not crash");
   const success = `Scoutr database ready on port ${credentials.port}`;
-  const hostError = `Postgress: Ensure your environment has a Postgres server. If using Docker, verify that the host called ${credentials.host} matches the Docker container name of the database`;
-  const portError = `Postgress: Check if the port ${credentials.port} matches the exposed port in Docker`;
-  const databaseError = `Postgress: The database called ${credentials.database} does not exist on the current Postgress server`;
-  const authError = `Postgress: The user ${credentials.user} or its password do not match the records in the database called ${credentials.database}`;
+  const hostError = `Postgres: Ensure your environment has a Postgres server. If using Docker, verify that the host called ${credentials.host} matches the Docker container name of the database`;
+  const portError = `Postgres: Check if the port ${credentials.port} matches the exposed port in Docker`;
+  const databaseError = `Postgres: The database called ${credentials.database} does not exist on the current Postgress server`;
+  const authError = `Postgres: The user ${credentials.user} or its password do not match the records in the database called ${credentials.database}`;
 
   try {
     await client.connect();
 
     console.info(success);
   } catch (error) {
-    if (error.code === "ENOTFOUND") console.error(hostError);
-    if (error.code === "ECONNREFUSED") console.error(portError);
-    if (error.code === "3D000") console.error(databaseError);
-    if (error.code === "28P01") console.error(authError);
+    if (error.code === "ENOTFOUND") throw new Error(hostError);
+    if (error.code === "ECONNREFUSED") throw new Error(portError);
+    if (error.code === "3D000") throw new Error(databaseError);
+    if (error.code === "28P01") throw new Error(authError);
 
     throw new Error(error);
   }
---
+++ backend/src/database/postgressClient.test.ts
@@ -0,0 +1,24 @@
+// Node modules
+import { expect, test } from "vitest";
+
+// Project files
+import postgresClient from "./postgresClient";
+import type DatabaseCredentials from "../types/DatabaseCredentials";
+
+test("Not having a Postgress server running shows a descriptive error", async () => {
+  // Arrange
+  const credentials: DatabaseCredentials = {
+    host: "test_host",
+    port: 1234,
+    database: "test_database",
+    user: "test_user",
+    password: "test_passwrod",
+  };
+  const result = `Postgres: Ensure your environment has a Postgres server. If using Docker, verify that the host called test_host matches the Docker container name of the database`;
+
+  // Act
+  const test = () => postgresClient(credentials);
+
+  // Assert
+  await expect(test).rejects.toThrowError(result);
+});
