65d50f539d787415ae89a930b9732403fce63ee7
Eduardo <el_alienx@hotmail.com>
2024-08-07 10:23:03 +0200

    added comment and removed unused function

--- backend/src/scripts/unZipLinks.ts
+++ backend/src/scripts/unZipLinks.ts
@@ -1,6 +1,19 @@
+/**
+ * Checks if the links received are an array.
+ * This is neccesary as the frontend converts 1 item array into a single string.
+ *
+ * Example 1 (normal):
+ *  - Frontend = ["www.eduardo.com", "www.alexia.com"] // sends an array with 2 strings
+ *  - Backend = ["www.eduardo.com", "www.alexia.com"] // receibes the same array with 2 strings
+ *
+ * Example 2 (the problem):
+ * - Frontend = ["www.eduardo.com"] // sends an array with 1 string
+ * - Backend  = "www.eduardo.com" // receives only the string
+ *
+ */
 export default function unZipLinks(links: string | string[]): string[] {
   // Safeguard
-  if (links === "") throw new Error("The query received was empty");
+  if (links === "") throw new Error("The query is empty");
 
   // Properties
   const result = Array.isArray(links) ? links : [links];
--- backend/src/scripts/waitForSeconds.ts
+++
@@ -1,5 +0,0 @@
-export default async function waitForSeconds(seconds: number) {
-  const secondToMiliseconds = 1000;
-
-  await new Promise((resolve) => setTimeout(resolve, seconds * secondToMiliseconds));
-}
