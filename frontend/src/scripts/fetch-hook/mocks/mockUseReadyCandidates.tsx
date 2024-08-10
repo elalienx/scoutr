b09912f6d6b1192ccbecf89d6ed5064ff23cfc23
Eduardo <el_alienx@hotmail.com>
2024-08-10 14:27:42 +0200

    added hardcoded images to mock and Cosmos so they dont expire when LinkedIn update its server images

--- frontend/src/scripts/fetch-hook/mocks/mockUseReadyCandidates.tsx
+++ frontend/src/scripts/fetch-hook/mocks/mockUseReadyCandidates.tsx
@@ -1,5 +1,6 @@
 // Project files
 import ResultsAPI from "types/ResultAPI";
+import SampleImages from "./sample-images.json";
 
 export default function mockUseReadyCandidates(uri: string): ResultsAPI {
   return {
@@ -8,14 +9,11 @@ export default function mockUseReadyCandidates(uri: string): ResultsAPI {
         id: 1,
         candidate_name: "Eduardo Alvarez Nowak",
         candidate_job_title: "Tech Lead",
-        candidate_image_url:
-          "https://media.licdn.com/dms/image/C4E03AQHwKgpnjrXkZA/profile-displayphoto-shrink_400_400/0/1643017689481?e=1718841600&v=beta&t=oXzvwBCY0QRE9ZnWl5CCqyBmDZFS2c_Jk9fDpndKzf8",
+        candidate_image_url: SampleImages.candidate_eduardo,
         company_name: "Novare Potential",
         company_duration_in_months: 34,
-        company_image_url:
-          "https://media.licdn.com/dms/image/C4E0BAQHElmOdWZ-xZA/company-logo_100_100/0/1631374829245/novare_potential_logo?e=1721260800&v=beta&t=ZRqH0M228v3G2tsbV5UsqqbmXstjR5_GQ69QLuw0eR8",
-        notes:
-          "Highly qualified, likes to help people and stays up to date with the latest frontend development tools.",
+        company_image_url: SampleImages.company_novare,
+        notes: `Highly qualified, likes to help people and stays up to date with the latest frontend development tools.`,
         relevance: 3,
         contact_status: 4,
         contact_date: "2024-04-02 21:00:30.610279",
@@ -27,12 +25,10 @@ export default function mockUseReadyCandidates(uri: string): ResultsAPI {
         id: 2,
         candidate_name: "Sussana Vara",
         candidate_job_title: "Partner",
-        candidate_image_url:
-          "https://media.licdn.com/dms/image/D5603AQF_QPzYf-dJDg/profile-displayphoto-shrink_400_400/0/1697639168537?e=1719446400&v=beta&t=_BQWtHxKlLQYZQi1YSwkZLNaeEUsYvCtwz7Ck0Jr-NA",
+        candidate_image_url: SampleImages.candidate_susanna,
         company_name: "Novare Potential",
         company_duration_in_months: 80,
-        company_image_url:
-          "https://media.licdn.com/dms/image/C4E0BAQHElmOdWZ-xZA/company-logo_100_100/0/1631374829245/novare_potential_logo?e=1721260800&v=beta&t=ZRqH0M228v3G2tsbV5UsqqbmXstjR5_GQ69QLuw0eR8",
+        company_image_url: SampleImages.company_novare,
         notes: "A servant type of leader.",
         relevance: 4,
         contact_status: 1,
@@ -45,14 +41,11 @@ export default function mockUseReadyCandidates(uri: string): ResultsAPI {
         id: 3,
         candidate_name: "Lana Haddad",
         candidate_job_title: "Senior Talent Acquisition Specialist",
-        candidate_image_url:
-          "https://media.licdn.com/dms/image/C5603AQEhqKQ3aIFArw/profile-displayphoto-shrink_400_400/0/1580204916994?e=1719446400&v=beta&t=kZxSDBsc0G7Lx1HuQwemHuM7CHCvzi22VmLkd7XDvgI",
+        candidate_image_url: "",
         company_name: "Novare Potential",
         company_duration_in_months: 85,
-        company_image_url:
-          "https://media.licdn.com/dms/image/C4E0BAQHElmOdWZ-xZA/company-logo_100_100/0/1631374829245/novare_potential_logo?e=1721260800&v=beta&t=ZRqH0M228v3G2tsbV5UsqqbmXstjR5_GQ69QLuw0eR8",
-        notes:
-          "More than 8 years of HR recruitment experience in multiples industries like tech, restaurants, health sector, education, white collar, blue collar, executive search, and more. Great salary negotiation skills.",
+        company_image_url: SampleImages.company_novare,
+        notes: `More than 8 years of HR recruitment experience in multiples industries like tech, restaurants, health sector, education, white collar, blue collar, executive search, and more. Great salary negotiation skills.`,
         relevance: 5,
         contact_status: 0,
         contact_date: "2024-04-02 21:00:30.610279",
