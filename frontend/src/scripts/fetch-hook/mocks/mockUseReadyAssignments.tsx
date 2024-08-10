c311491302628d766e6c7ae7b1f82be17c434e92
Eduardo <el_alienx@hotmail.com>
2024-08-10 14:33:15 +0200

    fix broken tests, thanks CI for catching this

--- frontend/src/scripts/fetch-hook/mocks/mockUseReadyAssignments.tsx
+++ frontend/src/scripts/fetch-hook/mocks/mockUseReadyAssignments.tsx
@@ -16,7 +16,7 @@ export default function mockUseReadyAssignments(uri: string): ResultsAPI {
         id: 2,
         date_created: "2024-12-31",
         assignment_name: "Master Data Specialist",
-        company_name: "",
+        company_name: "McDonalds",
         company_image_url: SampleImages.company_mcdonalds,
       },
     ],
