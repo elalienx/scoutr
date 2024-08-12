d60d9df52a8c5a4290fb98398815ca2aeb814415
Eduardo <el_alienx@hotmail.com>
2024-08-12 10:14:09 +0200

    improved the big o notation of tableIndexes by using an object with the candidate id as the key rather than an array with objects inside.

--- frontend/src/scripts/table-sorting/memorizeIndexes.ts
+++ frontend/src/scripts/table-sorting/memorizeIndexes.ts
@@ -3,16 +3,23 @@ import type Candidate from "types/Candidate";
 import type TableIndex from "types/TableIndex";
 
 /**
- * Because we cannot promise the database will give sequential id's to candidates, when users are adding and deleting candidates on multiple projects,
- * This table will generate a table index to display that will keep the index instact, even if you sort the table.
+ * About:
+ * Because we cannot promise the database will give sequential id's to candidates,
+ * when users are adding and deleting candidates on multiple projects,
+ * This table will generate a table index to display that will keep the index instact,
+ * even if you sort the table.
+ *
+ * @param candidates
+ * @returns TableIndex
  */
-export default function memorizeIndexes(candidates: Candidate[]): TableIndex[] {
+export default function memorizeIndexes(candidates: Candidate[]): TableIndex {
   const clonnedArray = [...candidates];
   const orderedCandidates = clonnedArray.sort((a, b) => a.id - b.id);
-  const result = orderedCandidates.map((item, index) => ({
-    candidate_id: item.id,
-    table_index: index + 1,
-  }));
+  const result: TableIndex = {};
+
+  orderedCandidates.forEach((candidate, index) => {
+    result[candidate.id] = index + 1;
+  });
 
   return result;
 }
--- frontend/src/types/TableIndex.ts
+++ frontend/src/types/TableIndex.ts
@@ -1,11 +1,10 @@
 /**
  * Because we cannot promise the database will give sequential id's to candidates, when users are adding and deleting candidates on multiple projects,
  * This table will generate a table index to display that will keep the index instact, even if you sort the table.
+ *
+ * Key "candidate_id": The original candidate id to use as a lookup function.
+ * Value: The order that it was added to the table when created.
  */
 export default interface TableIndex {
-  /** The original candidate id to use as a lookup function. */
-  candidate_id: number;
-
-  /** The order that it was added to the table when created. */
-  table_index: number;
+  [candidate_id: number]: number;
 }
