2d3c0120b8b11548f8509a7392dec11e18df3031
Eduardo <el_alienx@hotmail.com>
2024-08-14 10:08:08 +0200

    added line after enter key

--- frontend/src/scripts/forms/onPaste.ts
+++ frontend/src/scripts/forms/onPaste.ts
@@ -4,10 +4,11 @@ import removeQueryFromURL from "./removeQueryFromURL";
 export default function onPaste(event: React.ClipboardEvent<HTMLTextAreaElement>) {
   const pastedText = event.clipboardData.getData("text");
   const parsedURL = removeQueryFromURL(pastedText);
+  const addLineAfterPaste = "\n";
   const textarea = event.target as HTMLTextAreaElement;
   const start = textarea.selectionStart;
   const end = textarea.selectionEnd;
 
   event.preventDefault();
-  textarea.setRangeText(parsedURL, start, end, "end");
+  textarea.setRangeText(parsedURL + addLineAfterPaste, start, end, "end");
 }
