962671d87f960889ead337476f1201b744928a60
Eduardo <el_alienx@hotmail.com>
2024-08-16 15:30:05 +0200

    sending to CI

--- backend/src/scan-profile/transform/helpers/companyDurationInMonths.ts
+++ backend/src/scan-profile/transform/helpers/companyDurationInMonths.ts
@@ -5,10 +5,12 @@ import { CheerioAPI } from "cheerio";
 import extractJobDuration from "./extractJobDuration";
 import convertJobDurationToMonths from "./converJobDurationToMonths";
 
-export default function companyDurationInMonths(document: CheerioAPI): number {
+export default function companyDurationInMonths(document: CheerioAPI, profileType: number): number {
   const parentTag = "span.t-14.t-normal";
   const childTag = "span[aria-hidden='true']";
-  const htmlText = document(`${parentTag} > ${childTag}`).text();
+  const htmlText1 = document(`${parentTag} > ${childTag}`).text(); // if we put first() on the profile type 1 it breaks
+  const htmlText2 = document(`${parentTag} > ${childTag}`).first().text(); // to only bring the first job duration found otherwise it will return the other job durations in the same string messing up the math in the next step.
+  const htmlText = profileType === 1 ? htmlText1 : htmlText2;
   const jobDurationTrimmedText = extractJobDuration(htmlText);
   const result = convertJobDurationToMonths(jobDurationTrimmedText);
 
