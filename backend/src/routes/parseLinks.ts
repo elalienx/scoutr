29300c88f731bc809cee94d219f023bdf366b635
Eduardo <el_alienx@hotmail.com>
2024-08-18 22:01:47 +0200

    small documentation refactor

--- backend/src/routes/parseLinks.ts
+++ backend/src/routes/parseLinks.ts
@@ -5,7 +5,7 @@ import { Page } from "playwright";
 
 // Project files
 import etlProcess from "scan-profile/etlProcess";
-import enforceArray from "scripts/enforceArray";
+import enforceArray from "./helpers/enforceArray";
 
 export default async function parseLinks(request: Request, response: Response, database: Client, browserPage: Page) {
   // Headers
@@ -29,7 +29,7 @@ export default async function parseLinks(request: Request, response: Response, d
   } finally {
     response.end();
     // Important: To stop linkedin ad scripts eating resources
-    // The await is so we cannot parse more links until this process is done
+    // The await is to stop new network requests until this process is done
     await browserPage.goto("about:blank");
   }
 }
