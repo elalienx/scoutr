4b0831a86d28f9fc52d56667ddbeb4a1bf06b180
Eduardo <el_alienx@hotmail.com>
2024-08-23 22:07:27 +0200

    removing code

--- backend/src/scan-profile/transform/helpers/getProfileType.ts
+++ backend/src/scan-profile/transform/helpers/getProfileType.ts
@@ -2,10 +2,9 @@
 import { CheerioAPI } from "cheerio";
 
 export default function getProfileType(document: CheerioAPI): number {
-  const htmlTag = "a";
   const dataAttribute = "[data-field='experience_company_logo']";
   const cssSelector = ".full-width";
-  const uniqueProfile2Tag = document(htmlTag + dataAttribute + cssSelector).html();
+  const uniqueProfile2Tag = document(dataAttribute + cssSelector).html();
   const result = !uniqueProfile2Tag ? 1 : 2;
 
   return result;
