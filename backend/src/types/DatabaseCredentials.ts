8948bcbdfde9b2053baa7579ee8938428917372a
Eduardo Alvarez <el_alienx@hotmail.com>
2024-09-11 19:58:15 +0200

    added comments to type

--- backend/src/types/DatabaseCredentials.ts
+++ backend/src/types/DatabaseCredentials.ts
@@ -1,7 +1,16 @@
 export default interface DatabaseCredentials {
+  /** The IP address for the database. Is typically localhost when using Docker, as Docker manages the networking internally. */
   host: string;
+
+  /** The port number to connect to the host. */
   port: number;
+
+  /** The name of the database. */
   database: string;
+
+  /** The admin credential. */
   user: string;
+
+  /** The admin password. */
   password: string;
 }
